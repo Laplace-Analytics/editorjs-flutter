84d1a269bf0176edc319c2e04bf43b42aa46866f
Adnan Can TURKAY <32558546+canturkay@users.noreply.github.com>
2022-07-29 17:43:04 +0300

    Update EditorJSBlockData.dart

--- lib/src/model/EditorJSBlockData.dart
+++ lib/src/model/EditorJSBlockData.dart
@@ -10,17 +10,20 @@ class EditorJSBlockData {
   final bool? withBorder;
   final bool? stretched;
   final bool? withBackground;
+  final Map<String, dynamic> allFields;
 
-  EditorJSBlockData(
-      {this.text,
-      this.level,
-      this.style,
-      this.items,
-      this.file,
-      this.caption,
-      this.withBorder,
-      this.stretched,
-      this.withBackground});
+  EditorJSBlockData({
+    this.text,
+    this.level,
+    this.style,
+    this.items,
+    this.file,
+    this.caption,
+    this.withBorder,
+    this.stretched,
+    this.withBackground,
+    this.allFields = const <String, dynamic>{},
+  });
 
   factory EditorJSBlockData.fromJson(Map<String, dynamic> parsedJson) {
     var list = parsedJson['items'] as List?;
@@ -33,15 +36,15 @@ class EditorJSBlockData {
     }
 
     return EditorJSBlockData(
-        text: parsedJson['text'],
-        level: parsedJson['level'],
-        style: parsedJson['style'],
-        items: itemsList,
-        file: (parsedJson['file'] != null)
-            ? EditorJSBlockFile.fromJson(parsedJson['file'])
-            : null,
-        caption: parsedJson['caption'],
-        withBorder: parsedJson['withBorder'],
-        withBackground: parsedJson['withBackground']);
+      text: parsedJson['text'],
+      level: parsedJson['level'],
+      style: parsedJson['style'],
+      items: itemsList,
+      file: (parsedJson['file'] != null) ? EditorJSBlockFile.fromJson(parsedJson['file']) : null,
+      caption: parsedJson['caption'],
+      withBorder: parsedJson['withBorder'],
+      withBackground: parsedJson['withBackground'],
+      allFields: parsedJson,
+    );
   }
 }
