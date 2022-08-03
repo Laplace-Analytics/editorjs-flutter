893e613787a7c7b852a16ca697e56a4cedb33cb0
EyupEnsarKus1 <eyup@finfree.co>
2022-08-03 14:44:43 +0300

    test width added

--- lib/src/widgets/view.dart
+++ lib/src/widgets/view.dart
@@ -80,9 +80,12 @@ class EditorJSViewState extends State<EditorJSView> {
                 ));
                 break;
               case "paragraph":
-                items.add(Html(
-                  data: element.data!.text,
-                  style: customStyleMap,
+                items.add(SizedBox(
+                  width: MediaQuery.of(context).size.width*.8,
+                  child: Html(
+                    data: element.data!.text,
+                    style: customStyleMap,
+                  ),
                 ));
                 break;
               case "list":
